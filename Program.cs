5ec454f81994194dba28eb72a806323f19816361
Satqu <ezoss617@gmail.com>
2025-02-05 22:26:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введiть початкове значення Xmin: ");
+            double cosinys = 1;
+            string sxMin = Console.ReadLine();
+            double xMin = Double.Parse(sxMin);
+            Console.Write("Введiть кiнцеве значення Xmax: ");
+            string sxMax = Console.ReadLine();
+            double xMax = double.Parse(sxMax);
+            Console.Write("Введiть прирiст dX: ");
+            string sdx = Console.ReadLine();
+            double dx = double.Parse(sdx);
+            double x = xMin;
+            double y;
+            while (x <= xMax)
+            {
+                y = Math.Log(x*3)/Math.Pow(0.6 * x * Math.Sin(x*3) * Math.Cos(Math.Pow(x, 4)), 1.0 / 5.0);
+                cosinys *= Math.Cos(y); 
+                Console.WriteLine("x = {0}\t\t y = {1}", x, y);
+                x += dx;
+            }
+            if (Math.Abs(x - xMax - dx) > 0.0001)
+            {
+                y = Math.Log(x*3)/Math.Pow(0.6 * x * Math.Sin(x*3) * Math.Cos(Math.Pow(x, 4)), 1.0 / 5.0);
+                cosinys *= Math.Cos(y); 
+                Console.WriteLine("x = {0}\t\t y = {1}", xMax, y);
+            }
+            Console.WriteLine(cosinys);
+        }
+    }
+}
+
+
